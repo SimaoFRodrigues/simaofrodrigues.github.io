d08dd9ea71f39414cd6c8c136c4d9004d35f6567
ayosimao <simao3175@gmail.com>
2025-10-07 00:12:21 +0100

    Adição de mensagem ao clicar no botao de enviar mensagem, nao guarda os dados do lado do server porque ele não existe

---
+++ js/script.js
@@ -0,0 +1,5 @@
+function enviarMensagem() {
+  const email = document.getElementById(exampleFormControlInput1);
+  const msg = document.getElementById(exampleFormControlTextarea1);
+  alert("Mensagem guardada!");
+}
