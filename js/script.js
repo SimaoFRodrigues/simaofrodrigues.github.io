021f1443147b9832433674486b25e44b5ce7168c
ayosimao <simao3175@gmail.com>
2025-10-08 16:11:59 +0100

    toques finais v1

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 function guardarMensagem() {
   const email = document.getElementById("exampleFormControlInput1").value;
   const mensagem = document.getElementById("exampleFormControlTextarea1").value;
-  alert("Mensagem enviada com sucesso! 🎉");
+  alert("Mensagem enviada com sucesso!");
 }
