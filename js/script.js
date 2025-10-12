96fcc4c482db893b1e2b722dcc259d2eba45e1ef
ayosimao <simao3175@gmail.com>
2025-10-12 20:21:54 +0100

    eliminação do javascript não usado e de um componente do main.css que já não era necessário

--- js/script.js
+++
@@ -1,5 +0,0 @@
-function guardarMensagem() {
-  const email = document.getElementById("exampleFormControlInput1").value;
-  const mensagem = document.getElementById("exampleFormControlTextarea1").value;
-  alert("Mensagem enviada com sucesso!");
-}
